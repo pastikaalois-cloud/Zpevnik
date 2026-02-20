ffc6d6460723971ab99b04baade87c6870284ea6
pastikaalois-cloud <pastikaalois@gmail.com>
2026-02-20 21:22:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
 self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request, { cache: 'no-cache' })
+      .catch(() => caches.match(event.request))
+  );
+});
